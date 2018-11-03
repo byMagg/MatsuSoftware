922e107779b3426fd6d93aa41ab7952291fea918
byMagg <44727429+byMagg@users.noreply.github.com>
2018-11-03 15:12:06 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<?php
+    echo   '<footer>
+                <div id="logofooter"><img src="images/matsulogo.png" alt="matsulogo"></div>
+
+                <div id="linksfooter">
+                    <ul>
+                        <li><a href="contact.php">Contacto</a></li>
+                        <li><a href="tyc.php">Términos y condiciones</a></li>
+                        <li><a href="privacy.php">Privacidad</a></li>
+                    </ul>
+                </div>
+
+                <div id="rrssfooter">
+                    <ul>
+                        <li><a class="nohover" href="https://goo.gl/P7shbP"><img src="images/gp.png" alt="googleplayicon"></a></li>
+                        <li><a class="nohover" href="https://goo.gl/NAoba2"><img src="images/yt.png" alt="youtubeicon"></a></li>
+                    </ul>
+                </div>
+            </footer>';
+?>
---
+++ js/menu.js
@@ -0,0 +1,22 @@
+function toggleNav(){
+    var nav = document.getElementById("nav"), maxH = "150px";
+
+    if(nav.style.height == maxH){
+        nav.style.height = "0px";
+    }else{
+        nav.style.height = maxH;
+    }
+}
+ 
+/*
+window.addEventListener("mouseup", function(event){
+    if(event.target != nav && event.target.parentNode != nav){
+        nav.style.height = "0px";
+    }
+});
+
+*/
+
+
+
+
