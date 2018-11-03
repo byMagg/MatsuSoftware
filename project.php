4f38302e69cac3e348d5ab0520e71c9ab2bafd01
byMagg <byMag@hotmail.es>
2018-11-03 23:11:32 +0100

    cambios proyecto

--- project.php
+++ project.php
@@ -21,7 +21,9 @@
                 <h1>PROYECTOS FUTUROS:</h1>
                 <div id="proyectos">
                     <div class="item"><img src="images/paisaje.jpg" alt=""><h2>Lorem ipsum</h2><p>Fusce augue felis, bibendum in eleifend nec, egestas non justo. Nulla finibus interdum tortor. In hac habitasse platea dictumst. Aliquam erat volutpat. Integer sagittis sagittis ante eu convallis. In ipsum ante, sodales ac turpis in, laoreet tincidunt neque. Morbi hendrerit fermentum suscipit. Vestibulum et sem a libero viverra elementum. Sed ornare ante vel magna molestie porttitor.</p></div>
+                    <div class="linea"></div>
                     <div class="item"><img src="images/paisaje.jpg" alt=""><h2>Lorem ipsum</h2><p>Fusce augue felis, bibendum in eleifend nec, egestas non justo. Nulla finibus interdum tortor. In hac habitasse platea dictumst. Aliquam erat volutpat. Integer sagittis sagittis ante eu convallis. In ipsum ante, sodales ac turpis in, laoreet tincidunt neque. Morbi hendrerit fermentum suscipit. Vestibulum et sem a libero viverra elementum. Sed ornare ante vel magna molestie porttitor.</p></div>
+                    <div class="linea"></div>
                     <div class="item"><img src="images/paisaje.jpg" alt=""><h2>Lorem ipsum</h2><p>Fusce augue felis, bibendum in eleifend nec, egestas non justo. Nulla finibus interdum tortor. In hac habitasse platea dictumst. Aliquam erat volutpat. Integer sagittis sagittis ante eu convallis. In ipsum ante, sodales ac turpis in, laoreet tincidunt neque. Morbi hendrerit fermentum suscipit. Vestibulum et sem a libero viverra elementum. Sed ornare ante vel magna molestie porttitor.</p></div>
                 </div>
             </div>
