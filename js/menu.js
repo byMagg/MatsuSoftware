fc83a71e02ccd543adee2daf3c1f337185817783
byMagg <byMag@hotmail.es>
2018-11-04 09:45:45 +0100

    prueba

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,28 @@
-function toggleNav(){
-    var nav = document.getElementById("nav"), maxH = "150px";
-
-    if(nav.style.height == maxH){
-        nav.style.height = "0px";
-    }else{
-        nav.style.height = maxH;
-    }
-}
- 
-/*
-window.addEventListener("mouseup", function(event){
-    if(event.target != nav && event.target.parentNode != nav){
-        nav.style.height = "0px";
-    }
-});
-
-*/
-
-
-
-
+function toggleNav() {
+    var nav = document.getElementById("nav"),
+        maxH = "150px";
+
+    if (nav.style.height == maxH) {
+        nav.style.height = "0px";
+    } else {
+        nav.style.height = maxH;
+    }
+}
+
+
+
+/*
+
+window.addEventListener("mouseup", function(event){
+
+    if(event.target != nav && event.target.parentNode != nav){
+
+        nav.style.height = "0px";
+
+    }
+
+});
+
+
+
+*/
