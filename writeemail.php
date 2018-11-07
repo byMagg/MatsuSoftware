f81d563a96020c454134225ec6c369e6b04f86da
byMagg <byMag@hotmail.es>
2018-11-07 15:16:37 +0100

    cambios

--- writeemail.php
+++ writeemail.php
@@ -15,6 +15,7 @@
         <!--CONTENT-->
         <div class="content">
             <div id="content" class="content-inside">
+                <h1>RECUPERACIÓN DE CONTRASEÑA:</h1>
                 <div class="success">
                     <span>Siga los pasos del mensaje que se ha enviado a su correo.</span>
                 </div>
