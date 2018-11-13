05a0927fb2eb8daf418acf63ea93448f2018cddc
byMagg <byMag@hotmail.es>
2018-11-13 20:45:15 +0100

    cambios

--- videojuego.php
+++ videojuego.php
@@ -32,26 +32,27 @@
                             <p>Donec velit lectus, venenatis sed consequat sodales, tincidunt vel nunc. Aliquam et molestie lectus. Nulla non libero nulla. Integer eget lacinia nisl.</p>
                         </div>
                         <div>
-                            <iframe width="800" height="315" src="https://www.youtube.com/embed/GM7_we-jpfc?autoplay=1"></iframe>
+                            <iframe width="800" height="315" src="https://www.youtube.com/embed/GM7_we-jpfc?rel=0&autoplay=1&mute=1&loop=1&playlist=GM7_we-jpfc"></iframe>
                         </div>
                     </div>
                 </div>
+                <div id="linea"></div>
                 <div id="comentarios">
                         <div id="h1comentario"><h1>COMENTARIOS</h1></div>
                         <div class="item">
-                            <div class="info"><a href="videojuego.php"><img src="images/paisaje.jpg" alt=""></a><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
+                            <div class="info"><img src="images/paisaje.jpg" alt=""><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
                             <div class="estrella"><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""></div>
                         </div>
                         <div class="item">
-                            <div class="info"><a href="videojuego.php"><img src="images/paisaje.jpg" alt=""></a><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
+                            <div class="info"><img src="images/paisaje.jpg" alt=""><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
                             <div class="estrella"><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""></div>
                         </div>
                         <div class="item">
-                            <div class="info"><a href="videojuego.php"><img src="images/paisaje.jpg" alt=""></a><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
+                            <div class="info"><img src="images/paisaje.jpg" alt=""><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
                             <div class="estrella"><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""></div>
                         </div>
                         <div class="item">
-                            <div class="info"><a href="videojuego.php"><img src="images/paisaje.jpg" alt=""></a><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
+                            <div class="info"><img src="images/paisaje.jpg" alt=""><div><h2>USUARIO</h2><h4>Opinión</h4></div></div>
                             <div class="estrella"><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""><img src="images/estrella.png" alt=""></div>
                         </div>
                 </div>
