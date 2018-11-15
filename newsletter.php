d7d2322c67b57e1e1a7d3a7ed33515cc99357543
soulbro1998 <jueshi1998@gmail.com>
2018-11-15 18:09:32 +0100

    Newsletter

--- newsletter.php
+++ newsletter.php
@@ -13,7 +13,36 @@
         <?php require "views/header.php"; ?>
         <!--CONTENT-->
         <div class="content">
-        
+            <div id="content" class="content-inside">
+                <div id="cabecera">
+                    <h1>NEWSLETTER</h1>
+                </div>
+
+                <div id="cuadricula">                
+                    <select name="orden" required>
+                        <option disabled selected>Ordenación</option>
+                        <option value='asc'>Orden Ascendente</option>
+                        <option value='desc'>Orden Descendente</option>
+                    </select>
+                </div>
+                
+                <div id="tabla">
+                    <table>
+                        <tr>
+                            <td><img class="delete" src="images/paisaje.jpg" alt="Usuario"></td>
+                            <td>Vestibulum commodo sed sem non pellentesque. In arcu metus, efficitur eu velit vel, imperdiet interdum magna. Praesent mollis arcu felis. Integer scelerisque tortor eu vulputate convallis. Morbi vestibulum viverra neque, a pulvinar elit dictum vel. Etiam elementum augue ac lacinia blandit. Donec sollicitudin tellus lacus, at ullamcorper tellus tempor vel. Nullam feugiat suscipit condimentum.</td>
+                            
+                            
+                        </tr>
+
+                        <tr>
+                            <td><img class="delete" src="images/paisaje.jpg" alt="Usuario"></td>
+                            <td>Vestibulum commodo sed sem non pellentesque. In arcu metus, efficitur eu velit vel, imperdiet interdum magna. Praesent mollis arcu felis. Integer scelerisque tortor eu vulputate convallis. Morbi vestibulum viverra neque, a pulvinar elit dictum vel. Etiam elementum augue ac lacinia blandit. Donec sollicitudin tellus lacus, at ullamcorper tellus tempor vel. Nullam feugiat suscipit condimentum.</td>
+                        </tr>
+                    </table>
+                </div>
+
+            </div>
         </div>
         <!-- FOOTER -->
         <?php require "views/footer.php"; ?>
