2ab3499a0d72831ed3fc413dcc23bc007dcb8e66
byMagg <byMag@hotmail.es>
2018-11-15 18:21:52 +0100

    arreglos newsletter

--- newsletter.php
+++ newsletter.php
@@ -15,7 +15,7 @@
         <div class="content">
             <div id="content" class="content-inside">
                 <div id="cabecera">
-                    <h1>NEWSLETTER</h1>
+                    <h1>NEWSLETTER:</h1>
                 </div>
 
                 <div id="cuadricula">                
@@ -29,15 +29,13 @@
                 <div id="tabla">
                     <table>
                         <tr>
-                            <td><img class="delete" src="images/paisaje.jpg" alt="Usuario"></td>
-                            <td>Vestibulum commodo sed sem non pellentesque. In arcu metus, efficitur eu velit vel, imperdiet interdum magna. Praesent mollis arcu felis. Integer scelerisque tortor eu vulputate convallis. Morbi vestibulum viverra neque, a pulvinar elit dictum vel. Etiam elementum augue ac lacinia blandit. Donec sollicitudin tellus lacus, at ullamcorper tellus tempor vel. Nullam feugiat suscipit condimentum.</td>
-                            
-                            
+                            <td><img class="icono" src="images/paisaje.jpg" alt="Usuario"></td>
+                            <td><h4 class="fecha">18/11/2018</h4><p>Vestibulum commodo sed sem non pellentesque. In arcu metus, efficitur eu velit vel, imperdiet interdum magna. Praesent mollis arcu felis. Integer scelerisque tortor eu vulputate convallis. Morbi vestibulum viverra neque, a pulvinar elit dictum vel. Etiam elementum augue ac lacinia blandit. Donec sollicitudin tellus lacus, at ullamcorper tellus tempor vel. Nullam feugiat suscipit condimentum.</p></td>
                         </tr>
 
                         <tr>
-                            <td><img class="delete" src="images/paisaje.jpg" alt="Usuario"></td>
-                            <td>Vestibulum commodo sed sem non pellentesque. In arcu metus, efficitur eu velit vel, imperdiet interdum magna. Praesent mollis arcu felis. Integer scelerisque tortor eu vulputate convallis. Morbi vestibulum viverra neque, a pulvinar elit dictum vel. Etiam elementum augue ac lacinia blandit. Donec sollicitudin tellus lacus, at ullamcorper tellus tempor vel. Nullam feugiat suscipit condimentum.</td>
+                            <td><img class="icono" src="images/paisaje.jpg" alt="Usuario"></td>
+                            <td><h4 class="fecha">18/11/2018</h4><p>Vestibulum commodo sed sem non pellentesque. In arcu metus, efficitur eu velit vel, imperdiet interdum magna. Praesent mollis arcu felis. Integer scelerisque tortor eu vulputate convallis. Morbi vestibulum viverra neque, a pulvinar elit dictum vel. Etiam elementum augue ac lacinia blandit. Donec sollicitudin tellus lacus, at ullamcorper tellus tempor vel. Nullam feugiat suscipit condimentum.</p></td>
                         </tr>
                     </table>
                 </div>
