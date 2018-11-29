bbfbb654e1e6056b98c8e42970fe94d6a733d5e8
byMagg <byMag@hotmail.es>
2018-11-29 10:49:06 +0100

    arreglos

--- admindash.php
+++ admindash.php
@@ -22,8 +22,8 @@
                 <a id="cerrar" class="nohover" href="login/logout.php"><img src="images/logout.png" alt=""></a>
                 <div id="1" class="item"><a class="nohover" href="usermanagement.php"><img src="images/user.png" alt="Gestión de Usuarios"></a><a href="usermanagement.php" class="button">Gestión de Usuarios</a></div>
                 <div id="2" class="item"><a class="nohover" href="newslettermanagement.php"><img src="images/newsletter.png" alt="Gestion de NewsLetter"></a><a href="newslettermanagement.php" class="button">Gestión de Newsletter</a></div>
-                <div id="3" class="item"><a class="nohover" href="projectmanagement.php"><img src="images/stats.png" alt="Gestión de Proyectos"></a><a href="" class="button">Gestión de Proyectos</a></div>
-                <div id="4" class="item"><a class="nohover" href="videogamemanagement.php"><img src="images/games.png" alt="Gestión de Videojuegos"></a><a href="gestionVideojuego.php" class="button">Gestión de Videojuegos</a></div>
+                <div id="3" class="item"><a class="nohover" href="projectmanagement.php"><img src="images/stats.png" alt="Gestión de Proyectos"></a><a href="projectmanagement.php" class="button">Gestión de Proyectos</a></div>
+                <div id="4" class="item"><a class="nohover" href="videogamemanagement.php"><img src="images/games.png" alt="Gestión de Videojuegos"></a><a href="videogamemanagement.php" class="button">Gestión de Videojuegos</a></div>
                 <div id="5" class="item"><a class="nohover" href="merchandisingmanagement.php"><img src="images/merchan.png" alt="Gestión de Merchandising"></a><a href="merchandisingmanagement.php" class="button">Gestión de Merchandising</a></div>
             </div>
         </div>
--- views/head.php
+++ views/head.php
@@ -2,15 +2,12 @@
 
     echo '  
             <!-- Favicon -->
-            <link rel="icon" type="image/png" href="images/favicon.png"/>    
+            <link rel="icon" type="image/png" href="images/pino.jpeg"/>    
 
             <!-- Meta -->
             <meta name="theme-color" content="#343F55"/>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
-            <!-- FuenteLetra -->
-            <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
             <!-- Iconos -->
             <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css" rel="stylesheet">
