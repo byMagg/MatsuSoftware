8547f33e0be3b364dccc45cdf0a3776b53b46b83
alexgaro5 <alexgarohotmailcom@gmail.com>
2019-01-09 10:54:58 +0100

    up`date

--- views/head.php
+++ views/head.php
@@ -6,7 +6,7 @@
 
             <!-- Meta -->
             <meta name="theme-color" content="#343F55"/>
-            <meta charset="utf-8">
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
             <!-- Iconos -->
